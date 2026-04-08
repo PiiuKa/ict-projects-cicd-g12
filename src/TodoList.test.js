1ac74a6ef797f2ea5388d2a9f128edb3c20f803c
PiiuKa <157690096+PiiuKa@users.noreply.github.com>
2026-04-08 13:20:44 +0300

    add task test

--- src/TodoList.test.js
+++ src/TodoList.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import { TodoList } from './TodoList';
+import { render, screen } from "@testing-library/react";
+import { TodoList } from "./TodoList";
 
-it('renders No task found when the list is empty', () => {
+it("renders No task found when the list is empty", () => {
   render(<TodoList />);
   const linkElement = screen.getByText(/No task found/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+it("adds task to todo list", () => {
+  render(<TodoList list={["task"]} remove={() => {}} />);
+  const taskItem = screen.getByText(/task/i);
+  expect(taskItem).toBeInTheDocument();
 });
